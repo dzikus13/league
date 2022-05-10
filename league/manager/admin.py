60fc10e89e53c8afb816ad13928385d906a14cfd
piotr.kryska <piotr.kryska@home.eu>
2022-05-10 09:56:02 +0200

    Initial model version

--- league/manager/admin.py
+++ league/manager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import League
+
+class LeagueManger(admin.ModelAdmin):
+    list_display = ["name"]
+
+admin.site.register(League, LeagueManger)
--- league/manager/models.py
+++ league/manager/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class League(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Team(models.Model):
+    team_id = models.CharField(max_length=10)
+
+
+class SumOfPoints(models.Model):
+    pass
+
+
+class Result(models.Model):
+    pass
+
+
+class Match(models.Model):
+    time = models.DateTimeField()
+
+
+class TeamPlayer(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+    score = models.IntegerField()
+    nick = models.CharField(max_length=20)
+
+
+class EventType(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Event(models.Model):
+    time = models.DateTimeField()
+    player = models.ForeignKey(TeamPlayer, on_delete=models.CASCADE)
+    event_type = models.ForeignKey(EventType, on_delete=models.CASCADE)
+
