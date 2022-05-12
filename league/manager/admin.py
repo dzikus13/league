0da994ac1c48829412109735c9555e1cd0e47b0f
Shefour <main.shefour@protonmail.com>
2022-05-12 09:19:10 +0200

    added new models, properties et ce tera

--- league/manager/admin.py
+++ league/manager/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import League
+from .models import League, Match
 
 class LeagueManger(admin.ModelAdmin):
-    list_display = ["name"]
-
+    list_display = ["name","ended"]
 admin.site.register(League, LeagueManger)
+admin.site.register(Match)
--- league/manager/models.py
+++ league/manager/models.py
@@ -5,22 +5,53 @@ from django.db import models
 
 class League(models.Model):
     name = models.CharField(max_length=50)
+    points_for_win = 3
+    points_for_lost = 0
+    points_for_draw = 1
+
+    @property
+    def all_matches(self):
+        return self.match_set.all().count()
+
+    @property
+    def played_matches(self):
+        return self.match_set.filter(result__isnull=False).count()
+
+    @property
+    def ended(self):
+        # TODO: Implement conditions for ending a league
+        if self.all_matches == self.played_matches:
+            return True
+        else:
+            return False
+
+    @property
+    def winner(self):
+        # TODO: Implement condition for winning a league
+        pass
 
 
 class Team(models.Model):
     team_id = models.CharField(max_length=10)
+    matches_won = models.IntegerField()
+    matches_draw = models.IntegerField()
+    matches_lost = models.IntegerField()
 
+    @property
+    def sum_of_points(self):
+        return self.matches_won * League.points_for_win + self.matches_draw * League.points_for_draw + self.matches_lost * League.points_for_lost
 
-class SumOfPoints(models.Model):
-    pass
-
-
-class Result(models.Model):
-    pass
+    @property
+    def matches_teams_played(self):
+        return self.matches_won + self.matches_draw + self.matches_lost
 
 
 class Match(models.Model):
+    match_id = models.IntegerField()
     time = models.DateTimeField()
+    result = models.IntegerField()
+    teams = models.ManyToManyField(Team)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
 
 
 class TeamPlayer(models.Model):
@@ -37,4 +68,4 @@ class Event(models.Model):
     time = models.DateTimeField()
     player = models.ForeignKey(TeamPlayer, on_delete=models.CASCADE)
     event_type = models.ForeignKey(EventType, on_delete=models.CASCADE)
-
+    match_id = models.ForeignKey(Match, on_delete=models.CASCADE)
