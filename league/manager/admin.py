12f1d8effadedde27bfa9f4b8e1273753c61dfae
Shefour <main.shefour@protonmail.com>
2022-05-18 00:28:13 +0200

    few changes in models and added new things

--- league/manager/admin.py
+++ league/manager/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import League, Match
 
+
 class LeagueManger(admin.ModelAdmin):
-    list_display = ["name","ended"]
+    list_display = ["name", "is_ended"]
+
+
 admin.site.register(League, LeagueManger)
 admin.site.register(Match)
