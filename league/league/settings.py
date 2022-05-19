e04d0855f9adc44cbe2495096eac7da14b054302
Maksymilian Marys <maksymilian.marys@gmail.com>
2022-05-19 20:31:19 +0200

    stworzenie html'i dla error'ow

--- league/league/settings.py
+++ league/league/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from os import path
 
@@ -58,11 +58,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'league.urls'
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- league/manager/urls.py
+++ league/manager/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import leagues, manager, league_details
+
+handler404 = 'manager.views.error_404'
+handler500 = 'manager.views.error_500'
+# handler403 = 'manager.views.error_403'
+# handler400 = 'manager.views.error_400'
 
 
 urlpatterns = [
     path("", manager),
     path("leagues", leagues, name="league_list"),
     re_path("league/(?P<league_id>\d+)", league_details, name="league_details")
+
 ]
