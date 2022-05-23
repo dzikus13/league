826f460c16710e51e033bc60c594ecf361595fe3
Kornel <49289643+Shefour@users.noreply.github.com>
2022-05-23 09:38:30 +0200

    brakujący plik z modelami

--- league/manager/models.py
+++ league/manager/models.py
@@ -1,149 +1,72 @@
-from enum import Enum
 from django.core.exceptions import ValidationError
-from django.db import models
+from django.test import TestCase
+from django.utils import timezone
+
+# Create your tests here.
+
+from .models import League, Team, TeamPlayer, Match, Event
 
 
-class League(models.Model):
-    name = models.CharField(max_length=50)
-    points_for_win = models.IntegerField(default=3)
-    points_for_lost = models.IntegerField(default=0)
-    points_for_draw = models.IntegerField(default=1)
-    max_number_of_teams = models.IntegerField(default=10)
-    max_number_of_players_in_team = models.IntegerField(default=2)
+class LeagueTest(TestCase):
+    def test_league_adding(self):
+        self.assertEqual(League.objects.all().count(), 0)
+        league = League(name="Test league 1")
+        league.save()
+        self.assertEqual(League.objects.all().count(), 1)
+        l = League.objects.create(name="Test League 2")
+        self.assertEqual(League.objects.all().count(), 2)
 
-    @property
-    def teams_number(self):
-        return self.team_set.all().count()
-
-    @property
-    def all_matches(self):
-        return self.match_set.all().count()
-
-    @property
-    def played_matches(self):
-        return self.match_set.filter(event__isnull=False).count()
-
-    @property
-    def is_ended(self):
-        if self.all_matches > 0:
-            return self.all_matches == self.played_matches
-        else:
-            return False
-
-    @property
-    def league_winner(self):
-        if self.is_ended:
-            return max(self.team_set.all().sum_of_points)
-        else:
-            return None
-          
-
-class Team(models.Model):
-    league = models.ForeignKey(League, on_delete=models.CASCADE)
-    team_name = models.CharField(max_length=10)
-    matches_won = models.IntegerField(default=0)
-    matches_draw = models.IntegerField(default=0)
-    matches_lost = models.IntegerField(default=0)
-
-    def save(self, *args, **kwargs):
-        if self.league.teams_number >= self.league.max_number_of_teams:
-            raise ValidationError("Max number of teams exceeded", code="max_teams")
-        return super().save(*args, **kwargs)
-
-    @property  # TODO:Zuzannka77 add test to check if this property works properly
-    def sum_of_points(self):
-        return self.matches_won * self.league.points_for_win + \
-               self.matches_draw * self.league.points_for_draw + \
-               self.matches_lost * self.league.points_for_lost
-
-    @property  # TODO:Zuzannka77 add test to check if this property works properly
-    def matches_team_played(self):
-        return self.matches_won + \
-               self.matches_draw + \
-               self.matches_lost
-
-    @property
-    def players_number(self):
-        return self.teamplayer_set.all().count()
+    def test_max_league_teams(self):
+        self.assertEqual(League.objects.all().count(), 0)
+        league = League(name="Test league 1", max_number_of_teams=0)
+        league.save()
+        with self.assertRaises(ValidationError):
+            team = Team.objects.create(league=league, team_name="Test team name")
 
 
-class Match(models.Model):
-    league = models.ForeignKey(League, on_delete=models.CASCADE)
-    match_date = models.DateTimeField(default=timezone.now, blank=True)
-    match_duration = models.DurationField(default="01:30:00")
-    # team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team1')
-    # team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='team2')
+class LeagueAddedTest(TestCase):
+    def setUp(self):
+        self.league = League.objects.create(name="Test league")
+    def test_league_name(self):
+        self.assertEqual(self.league.name,"Test league")
 
-    def amount_gols_team1(self):
-        # TODO:elzbietagawickaLOVE Number of goals scored by the team1
-        # return Event.objects.filter(Event.event_type="gol").filter(Event.team = team1).count()
-        pass
+    def test_league_teams(self):
+        self.assertEqual(self.league.teams_number, 0)
 
-    def amount_gols_team2(self):
-        # TODO:elzbietagawickaLOVE Number of goals scored by the team2
-        # return Event.objects.filter(Event.event_type="gol").filter(Event.team = team2).count()
-        pass
+    def tearDown(self):
+        self.league.delete()
 
-    def winner(self):
-        # TODO:elzbietagawickaLOVE Winner in match
-        if self.drawn():
-            return None
-        else:
-            if self.amount_gols_team1 > self.amount_gols_team2:
-                return self.team1.team_name
-            else:
-                return self.team2.team_name
-
-    def loser(self):
-        # TODO:elzbietagawickaLOVE Loser in match
-        if self.drawn():
-            return None
-        else:
-            if self.amount_gols_team1 < self.amount_gols_team2:
-                return self.team1.team_name
-            else:
-                return self.team2.team_name
-
-    def drawn(self):
-        # TODO:elzbietagawickaLOVE Draw in match
-        return self.amount_gols_team1 == self.amount_gols_team2
+    def test_teams_number(self):
+        self.assertEqual(Team.objects.all().count(), 0)
+        self.team = Team.objects.create(league=self.league, team_name="Test team name")
+        self.assertEqual(Team.objects.all().count(), 1)
+        self.assertEqual(self.league.teams_number, 1)
+        self.league2 = League(name="Test league 2")
+        self.league2.save()
+        self.team2 = Team.objects.create(league=self.league2, team_name="Test 2")
+        self.assertEqual(Team.objects.all().count(), 2)
+        self.assertEqual(self.league.teams_number, 1)
+        self.assertEqual(self.league2.teams_number, 1)
 
 
-class TeamPlayer(models.Model):
-    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
-    player_nick = models.CharField(max_length=20)
+class MaxPlayersTest(TestCase):
+    def setUp(self):
+        self.league = League.objects.create(name="Test league")
 
-    @property
-    def goals(self):
-        # TODO: Number of goals scored by the player
-        pass
+    def test_league_name(self):
+        self.assertEqual(self.league.name,"Test league")
 
-    def save(self, *args, **kwargs):
-        if self.team.players_number >= self.team.league.max_number_of_players_in_team:
-            raise ValidationError("Max number of players in that team exceeded", code="max_players_in_team")
-        return super().save(*args, **kwargs)
+    def test_league_teams(self):
+        self.assertEqual(self.league.teams_number, 0)
 
+    def tearDown(self):
+        self.league.delete()
 
-class EventType(models.TextChoices):
-
-    MATCH_WON = "Match has been won"
-    MATCH_LOST = "Match has been lost"
-    MATCH_DRAW = "Match has been drawn"
-    MATCH_GOAL = "Goal has been scored"
-    MISSING = "Event not specified"
-
-
-class Event(models.Model):
-    event_type = models.CharField(max_length=20, choices=EventType.choices, default="MISSING")
-
-    match = models.ForeignKey(Match, on_delete=models.CASCADE)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
-    player = models.ForeignKey(TeamPlayer, on_delete=models.CASCADE)
-    event_time = models.DateTimeField(auto_now_add=True)
-
-    @classmethod
-    def get_events_for(cls, match, event_type=None):
-        ret_qs = cls.objects.filter(match=match)
-        if event_type is not None:
-            ret_qs = ret_qs.filter(event_type=event_type)
-        return ret_qs
+    def test_players_max_number(self):
+        self.assertEqual(Team.objects.all().count(), 0)
+        self.team = Team.objects.create(league=self.league, team_name="Test team name")
+        self.assertEqual(Team.objects.all().count(), 1)
+        self.assertEqual(self.league.teams_number, 1)
+        self.team_player = TeamPlayer.objects.create(team=self.team, player_nick="player1")
+        self.team_player = TeamPlayer.objects.create(team=self.team, player_nick="player2")
+        self.assertEqual(TeamPlayer.objects.all().count(), 2)
