0b067848b0f5b16d84a9c8edc0ac242b281534c1
Tomasz Janowiak <janowiaktomasz35@gmail.com>
2022-05-23 10:34:02 +0200

    Still incomplete!

--- league/manager/admin.py
+++ league/manager/admin.py
@@ -5,62 +5,30 @@ from .models import League, Match, Team, TeamPlayer, Event, EventType
 
 
 class LeagueManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("name")
-    list_display.append("ended")
-    list_display.append("points_for_win")
-    list_display.append("points_for_lost")
-    list_display.append("points_for_draw")
-    list_display.append("max_number_of_teams")
-    list_display.append("max_number_of_players_in_team")
-    list_display.append("teams_number")
-    list_display.append("all_matches")
-    list_display.append("played_matches")
-    list_display.append("league_winner")
+    list_display = ["name", "ended", "points_for_win", "points_for_lost", "points_for_draw", "max_number_of_teams",
+                    "max_number_of_players_in_team", "teams_number", "all_matches", "played_matches", "league_winner"]
 
 
 class TeamManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("team_name")
-    list_display.append("matches_won")
-    list_display.append("matches_draw")
-    list_display.append("matches_lost")
-    list_display.append("league")
-    list_display.append("sum_of_points")
-    list_display.append("matches_team_played")
-    list_display.append("players_number")
+    list_display = ["team_name", "matches_won", "matches_draw", "matches_lost", "league", "sum_of_points",
+                    "matches_team_played", "players_number"]
 
 
 class MatchManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("league")
-    list_display.append("match_duration")
+    list_display = ["league", "match_duration"]
 
 
 class TeamPlayerManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("team")
-    list_display.append("player_nick")
+    list_display = ["team", "player_nick"]
 
 
 class EventTypeManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("MATCH_WON")
-    list_display.append("MATCH_LOST")
-    list_display.append("MATCH_DRAW")
-    list_display.append("MATCH_GOAL")
-    list_display.append("MATCH_FIRST_YELLOW")
-    list_display.append("MATCH_SECOND_YELLOW")
-    list_display.append("MATCH_RED")
+    list_display = ["MATCH_WON", "MATCH_LOST", "MATCH_DRAW", "MATCH_GOAL", "MATCH_FIRST_YELLOW", "MATCH_SECOND_YELLOW",
+                    "MATCH_RED"]
 
 
 class EventManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("event_type")
-    list_display.append("match")
-    list_display.append("team")
-    list_display.append("player")
-    list_display.append("event_time")
+    list_display = ["event_type", "match", "team", "player", "event_time"]
 
 
 admin.site.register(League, LeagueManager)
