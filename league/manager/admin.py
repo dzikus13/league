e24c6322b30b7d5b33d4091925648df26ea74fb4
Szymon Swica <szymonswica575@gmail.com>
2022-05-27 10:23:26 +0200

    Added some css and user profile site.

--- league/manager/admin.py
+++ league/manager/admin.py
@@ -4,59 +4,31 @@ from django.contrib import admin
 from .models import League, Match, Team, TeamPlayer, Event, EventType
 
 class LeagueManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("name")
-    list_display.append("max_number_of_teams")
-    list_display.append("points_for_win")
-    list_display.append("points_for_lost")
-    list_display.append("points_for_draw")
-    list_display.append("teams_number")
-    list_display.append("all_matches")
-    list_display.append("played_matches")
-    list_display.append("ended")
-    list_display.append("winner")
+    list_display = ["name", "max_number_of_teams", "points_for_win", "points_for_lost", "points_for_draw",
+                    "teams_number", "all_matches", "played_matches", "ended", "winner"]
 
 
 class TeamManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("team_name")
-    list_display.append("matches_won")
-    list_display.append("matches_draw")
-    list_display.append("matches_lost")
-    list_display.append("league")
-    list_display.append("max_players_number")
-    list_display.append("number_of_players")
-    list_display.append("sum_of_points")
-    list_display.append("matches_teams_played")
+    list_display = ["team_name", "matches_won", "matches_draw", "matches_lost", "league", "max_players_number",
+                    "number_of_players", "sum_of_points", "matches_teams_played"]
 
 
 class MatchManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("time")
-    list_display.append("result")
+    list_display = ["time", "result", "league"]
     # list_display.append("teams") wywala blad
-    list_display.append("league")
     # list_display.append("save") to chyba nie jest nawet property ale sie nie znam na tym co backend zrobil
 
 
 class TeamPlayerManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("team")
-    list_display.append("score")
-    list_display.append("nick")
+    list_display = ["team", "score", "nick"]
 
 
 class EventTypeManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("name")
+    list_display = ["name"]
 
 
 class EventManager(admin.ModelAdmin):
-    list_display = []
-    list_display.append("time")
-    list_display.append("player")
-    list_display.append("event_type")
-    list_display.append("match")
+    list_display = ["time", "player", "event_type", "match"]
 
 
 admin.site.register(League, LeagueManager)
