9eebe0e38c8647b09000c41358dd4124310ebf1d
Maksymilian Marys <maksymilian.marys@gmail.com>
2022-05-28 19:13:12 +0200

    Resolved conflicts

---
+++ league/manager/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import League, Match, Team, TeamPlayer, Event, EventType
+
+class LeagueManager(admin.ModelAdmin):
+    list_display = ["name", "max_number_of_teams", "points_for_win", "points_for_lost", "points_for_draw",
+                    "teams_number", "all_matches", "played_matches"]
+
+
+class TeamManager(admin.ModelAdmin):
+    list_display = ["team_name", "league", "sum_of_points"]
+
+
+class MatchManager(admin.ModelAdmin):
+    list_display = ["league"]
+    # list_display.append("teams") wywala blad
+    # list_display.append("save") to chyba nie jest nawet property ale sie nie znam na tym co backend zrobil
+
+
+class TeamPlayerManager(admin.ModelAdmin):
+    list_display = ["team"]
+
+
+class EventManager(admin.ModelAdmin):
+    list_display = ["player", "event_type", "match"]
+
+
+admin.site.register(League, LeagueManager)
+admin.site.register(Team, TeamManager)
+admin.site.register(Match, MatchManager)
+admin.site.register(TeamPlayer, TeamPlayerManager)
+admin.site.register(Event, EventManager)
+
+
