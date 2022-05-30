421a6e14413a0eb467341e974226639243887a04
elzbietagawickaLOVE <105270323+elzbietagawickaLOVE@users.noreply.github.com>
2022-05-30 22:58:24 +0200

    small integration with the front end team

--- league/manager/templatetags/custom_tags.py
+++ league/manager/templatetags/custom_tags.py
@@ -2,3 +2,7 @@ from django import template
 from django.template.defaultfilters import stringfilter
 
 register = template.Library()
+
+@register.filter
+def is_judge(user):
+    return user.groups.filter(name="Judges").exists()
