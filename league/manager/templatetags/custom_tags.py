f84abad19d98afa27de65df3a17ddcfc0624fbd0
elzbietagawickaLOVE <makspodg77@gmail.com>
2022-05-31 22:57:27 +0200

    Page now checks if a user is a Judge to add event

--- league/manager/templatetags/custom_tags.py
+++ league/manager/templatetags/custom_tags.py
@@ -3,6 +3,12 @@ from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
+
 @register.filter
 def is_judge(user):
     return user.groups.filter(name="Judges").exists()
+
+
+@register.filter(name='group')
+def group(u, group_names):
+    return u.groups.filter(name=group_names)
